f98fa2a6d1143ba5bc51d34c79fc19ba05706667
Cintia Magali Gramajo <gramajomagali62@gmail.com>
2025-04-05 18:35:58 -0300

    Modificacion del archivo C

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,18 @@
 
-#include<stdio.h>  
-int main(){
-printf(hola mundo);
+#include <stdio.h>
+int main()
+{
+    printf("hola mundo");
+
+    int numero = 42;
+
+    int *puntero = &numero;
+
+    printf("Contenido del puntero: %d\n", *puntero);
+    printf("Direccion almacenada por el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &numero);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamaño de memoria de la variable: %zu bytes\n", sizeof(numero));
+
+    return 0;
 }
